fb00b15b7f6c07c44511830fedd2921c19d76469
Cecile Alfred <cecile.alfredx@gmail.com>
2019-05-12 22:08:03 +0100

    Functional Components Practice - created new component myInfo and cleaned App

--- src/index.js
+++ src/index.js
@@ -1,25 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 // The render method takes two arguments
 // What Do I Want To Render
 // Where Do I Want To Render It
 // --> Everything is inserted in an HTML page
-ReactDOM.render(
-	<ul>
-		<li>Item 1</li>
-		<li>Item 2</li>
-		<li>Item 3</li>
-	</ul>, document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
-// <ul></ul> is JSX, Javascript rendition of HTML
+// <App /> is JSX, Javascript rendition of HTML
 // we import the React library to enable the use of JSX
 
+function MyInfo() {
+	return (
+		<div>
+			<h1>Name</h1>
+			<p>paragraph to describe a person</p>
+			<ul>
+				<li>Vacation spot 1</li>
+				<li>Vacation spot 2</li>
+				<li>Vacation spot 3</li>
+			</ul>
+		</div>
+	);
+}
 
+// Observation: if I use render a second time, I don't see <App /> anymore
+// --> so I included <App /> in this call for the render!
+ReactDOM.render(<div><App /><MyInfo /></div>, document.getElementById('root'));
 
 
 
