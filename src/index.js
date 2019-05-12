047eb13a6caf95d310dfe4db17bbdaef7a7b3892
Cecile Alfred <cecile.alfredx@gmail.com>
2019-05-12 21:32:46 +0100

    Initial commit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// The render method takes two arguments
+// What Do I Want To Render
+// Where Do I Want To Render It
+// --> Everything is inserted in an HTML page
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
