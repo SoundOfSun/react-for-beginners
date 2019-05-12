16d53db2e88695aa1d3322abb021d285d8d98022
Cecile Alfred <cecile.alfredx@gmail.com>
2019-05-12 21:46:07 +0100

    ReactDOM & JSX Practice - insert some JSX inside the HTML from index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 // The render method takes two arguments
 // What Do I Want To Render
 // Where Do I Want To Render It
 // --> Everything is inserted in an HTML page
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<ul>
+		<li>Item 1</li>
+		<li>Item 2</li>
+		<li>Item 3</li>
+	</ul>, document.getElementById('root')
+);
+
+// <ul></ul> is JSX, Javascript rendition of HTML
+// we import the React library to enable the use of JSX
+
+
+
+
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
